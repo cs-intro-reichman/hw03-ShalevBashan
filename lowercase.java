612093592370558f72bbd39f6ab1fb73c5b0b637
Shalev Bashan <shalev.bashan@post.runi.ac.il>
2024-01-07 18:49:07 +0200

    reupdated files names after the autograder updated

--- lowercase.java
+++ lowercase.java
@@ -1,12 +1,12 @@
 
-public class lowercase {
+public class LowerCase {
     public static void main(String[] args) {  
         String str = args[0];
-        System.out.println(lowercase(str));
+        System.out.println(LowerCase(str));
     }
 
     
-    public static String lowercase(String s) {
+    public static String LowerCase(String s) {
         String lower = "";
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) <= 91 && s.charAt(i) >= 65) {
--- uniquechars.java
+++ uniquechars.java
@@ -1,11 +1,11 @@
-public class uniquechars {
+public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
-        System.out.println(uniquechars(str));
+        System.out.println(UniqueChars(str));
     }
 
 
-    public static String uniquechars(String s) {
+    public static String UniqueChars(String s) {
         String u = "";
         int cnt = 0;
         u += s.charAt(0);
